63b75f544142bb03871cf2ca1e16e0c6fc4a6e06
Johannes M Dieterich <johannes.dieterich@amd.com>
2022-07-07 13:58:22 -0500

    Implement FLA_Bidiag_apply_U_external_hip.c as a wrapper to rocSOLVER.

--- src/base/flamec/include/FLA_lapack_prototypes.h
+++ src/base/flamec/include/FLA_lapack_prototypes.h
@@ -219,6 +219,7 @@ FLA_Error FLA_Apply_pivots_unb_external_hip( rocblas_handle handle, FLA_Side sid
 FLA_Error FLA_Trsm_piv_external_hip( rocblas_handle handle, FLA_Obj A, void* A_hip, FLA_Obj B, void* B_hip, FLA_Obj p );
 
 FLA_Error FLA_Apply_Q_blk_external_hip( rocblas_handle handle, FLA_Side side, FLA_Trans trans, FLA_Store storev, FLA_Obj A, void* A_hip, FLA_Obj t, void* t_hip, FLA_Obj B, void* B_hip );
+FLA_Error FLA_Bidiag_apply_U_external_hip( rocblas_handle handle, FLA_Side side, FLA_Trans trans, FLA_Obj A, void* A_hip, FLA_Obj t, void* t_hip, FLA_Obj B, void* B_hip );
 #endif
 
 // --- check routine prototypes ------------------------------------------------
