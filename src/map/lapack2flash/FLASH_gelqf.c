4bddc6ca39661b744de451f449f82d207f58eca1
Michael Pittard <Michael.Pittard@amd.com>
2023-04-25 04:28:57 +0000

    First pass at adding FLASH calls

--- src/map/lapack2flash/FLASH_gelqf.c
+++ src/map/lapack2flash/FLASH_gelqf.c
@@ -28,35 +28,86 @@
                                PREFIX2LAPACK_TYPEDEF(prefix)* buff_w, int* lwork, \
                                int* info )
 
-#define LAPACK_gelqf_body(prefix)                               \
-  FLA_Datatype datatype = PREFIX2FLAME_DATATYPE(prefix);        \
-  FLA_Obj      A, t, T;                                         \
-  int          min_m_n  = min( *m, *n );                        \
-  FLA_Error    init_result;                                     \
-                                                                \
-  FLA_Init_safe( &init_result );                                \
-                                                                \
-  FLA_Obj_create_without_buffer( datatype, *m, *n, &A );        \
-  FLA_Obj_attach_buffer( buff_A, 1, *ldim_A, &A );              \
-                                                                \
-  FLA_Obj_create_without_buffer( datatype, min_m_n, 1, &t );    \
-  FLA_Obj_attach_buffer( buff_t, 1, min_m_n, &t );              \
-                                                                \
-  FLA_Set( FLA_ZERO, t );                                       \
-                                                                \
-  FLA_LQ_UT_create_T( A, &T );                                  \
-  FLA_LQ_UT( A, T );                                            \
-  FLA_LQ_UT_recover_tau( T, t );                                \
-  PREFIX2FLAME_INVERT_TAU(prefix,t);                            \
-                                                                \
-  FLA_Obj_free_without_buffer( &A );                            \
-  FLA_Obj_free_without_buffer( &t );                            \
-  FLA_Obj_free( &T );                                           \
-                                                                \
-  FLA_Finalize_safe( init_result );                             \
-                                                                \
-  *info = 0;                                                    \
-                                                                \
+#define LAPACK_gelqf_body(prefix)                                       \
+  FLA_Datatype datatype = PREFIX2FLAME_DATATYPE(prefix);                \
+  int          min_m_n  = min( *m, *n );                                \
+  FLA_Error    init_result;                                             \
+  dim_t        blocksize = FLASH_get_preferred_blocksize();             \
+                                                                        \
+  /*This is only for testing, once this works correctly, remove this*/  \
+  /*Set to 0 to "fix" one issue. Set to 1 to never use the FLA calls*/  \
+  int always_use_flash = 0;                                             \
+                                                                        \
+  FLA_Init_safe( &init_result );                                        \
+  if( *m <= *n || always_use_flash ){                                   \
+    FLA_Obj      A_flat, A, t, T;                                       \
+    /*I sent this up how I believe FLASH_LQ_UT() should be called*/     \
+    /*There seems to be an issue while running the netlibs-test 3.11*/  \
+    /*where if m > n, it fails and says */                              \
+    /*"libflame: Detected inconsistent object dimensions." */           \
+    /*Debug, remove this when fixed*/                                   \
+    /*fprintf(stderr,"FLASH: m=%d n=%d ldim_A=%d\n", *m, *n, *ldim_A);*/\
+    FLA_Obj_create_without_buffer( datatype, *m, *n, &A_flat );         \
+    FLA_Obj_attach_buffer( buff_A, 1, *ldim_A, &A_flat );               \
+                                                                        \
+    FLASH_Obj_create_without_buffer( datatype,                          \
+                                     min_m_n,                           \
+                                     1,                                 \
+                                     FLASH_get_depth(),                 \
+                                     &blocksize,                        \
+                                     &t );                              \
+    FLASH_Obj_attach_buffer( buff_t, 1, min_m_n, &t );                  \
+    FLASH_Set( FLA_ZERO, t );                                           \
+                                                                        \
+    FLASH_LQ_UT_create_hier_matrices( A_flat,                           \
+                                      FLASH_get_depth(),                \
+                                      &blocksize,                       \
+                                      &A,                               \
+                                      &T );                             \
+    FLASH_LQ_UT( A, T );                                                \
+    FLA_LQ_UT_recover_tau( T, t );                                      \
+    PREFIX2FLAME_INVERT_TAU(prefix,t);                                  \
+                                                                        \
+    FLA_Obj_free_without_buffer( &A_flat );                             \
+    FLASH_Obj_free_without_buffer( &t );                                \
+    FLASH_Obj_free( &T );                                               \
+    FLASH_Obj_free( &A );                                               \
+    /*This should possibly free without buffer, which might be a problem.*/     \
+    /*If FLASH_LQ_UT_create_hier_matrices() is making a copy of the buffer,*/   \
+    /*then this might not be properly manipulating the buffer data*/            \
+    /*I'm unsure of the buffer data of A needs to be copied back to A_flat*/    \
+    /*or if the buffer needs to be attached to A after it is made from A_flat*/ \
+    /*In one of the netlibs-tests in 3.11, './EIG/xeigtsts < se2.in'. It seems*/\
+    /*to loop for a long time until the test dies.*/                            \
+    /*gdb shows it seg fault at sdrvst2stg_ , but I suspect that*/              \
+    /*it's actually an issue of not updating the buffer correctly*/             \
+  }else{                                                                \
+    FLA_Obj      A, t, T;                                               \
+    /*If the issue where m>n breaking this gets fixed,*/                \
+    /*this block that defaults to FLA_ can be removed*/                 \
+    /*Debug, remove this when fixed*/                                   \
+    /*fprintf(stderr, "FLA: m=%d n=%d ldim_A=%d\n", *m, *n, *ldim_A);*/ \
+    FLA_Obj_create_without_buffer( datatype, *m, *n, &A );              \
+    FLA_Obj_attach_buffer( buff_A, 1, *ldim_A, &A );                    \
+                                                                        \
+    FLA_Obj_create_without_buffer( datatype, min_m_n, 1, &t );          \
+    FLA_Obj_attach_buffer( buff_t, 1, min_m_n, &t );                    \
+                                                                        \
+    FLA_Set( FLA_ZERO, t );                                             \
+                                                                        \
+    FLA_LQ_UT_create_T( A, &T );                                        \
+    FLA_LQ_UT( A, T );                                                  \
+    FLA_LQ_UT_recover_tau( T, t );                                      \
+    PREFIX2FLAME_INVERT_TAU(prefix,t);                                  \
+                                                                        \
+    FLA_Obj_free_without_buffer( &A );                                  \
+    FLA_Obj_free_without_buffer( &t );                                  \
+    FLA_Obj_free( &T );                                                 \
+  }                                                                     \
+  FLA_Finalize_safe( init_result );                                     \
+                                                                        \
+  *info = 0;                                                            \
+                                                                        \
   return 0;
 
 LAPACK_gelqf(s)
